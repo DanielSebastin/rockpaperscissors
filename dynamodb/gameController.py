4e75d5e03333740578bec27f60f542f14cad4090
Daniel Sebastin A <danielsebastinds@gmail.com>
2026-01-01 19:27:27 +0530

    Implement GameController for game management

---
+++ dynamodb/gameController.py
@@ -0,0 +1,52 @@
+import boto3
+from datetime import datetime
+
+class GameController:
+    def __init__(self,cm):
+        self.cm=cm
+        self.table=cm.get_table()
+
+    def create_game(self,gameId,host,opponent):
+        self.table.put_item(
+            Item={
+                "GameId":gameId,
+                "HostId":host,
+                "OpponentId":opponent,
+                "HostChoice":None,
+                "OpponentChoice":None,
+                "Result":None
+            }
+        )
+
+    def get_game(self,gameId):
+        res=self.table.get_item(Key={"GameId":gameId})
+        return res.get("Item")
+
+    def submit_choice(self,gameId,user,choice):
+        field="HostChoice" if self.get_game(gameId)["HostId"]==user else "OpponentChoice"
+        self.table.update_item(
+            Key={"GameId":gameId},
+            UpdateExpression=f"SET {field}=:c",
+            ExpressionAttributeValues={":c":choice}
+        )
+        self.resolve(gameId)
+
+    def resolve(self,gameId):
+        g=self.get_game(gameId)
+        h=g.get("HostChoice")
+        o=g.get("OpponentChoice")
+        if not h or not o:
+            return
+
+        if h==o:
+            result="Tie"
+        elif (h,o) in [("ROCK","SCISSORS"),("PAPER","ROCK"),("SCISSORS","PAPER")]:
+            result=g["HostId"]
+        else:
+            result=g["OpponentId"]
+
+        self.table.update_item(
+            Key={"GameId":gameId},
+            UpdateExpression="SET Result=:r",
+            ExpressionAttributeValues={":r":result}
+        )
